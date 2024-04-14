c406328be0d40f1b20d99421f13e0fcfc5b4c212
junho01px201 <junho01px201@kaist.ac.kr>
2024-04-14 17:58:25 +0900

    act06
    ..

--- Activities/Act06/Program.fs
+++ Activities/Act06/Program.fs
@@ -11,18 +11,18 @@ let cdr lst =
   | _ -> failwith "Empty list is given."
 
 /// Modify this function to return the last element of the given list.
-let last lst =
+let rec last lst =
   match lst with
-    | [] -> failwith "no last element"
-    | [x] -> x
-    | _ :: tail -> lastElement tail
+  | [] -> failwith "no last element"
+  | [x] -> x
+  | _ :: tail -> last tail
 
 /// Modify this function to return the last but one element of the given list.
-let lastButOne lst =
+let rec lastButOne lst =
   match lst with
-    | [] | [_] -> failwith "fewer than two elements"
-    | [x; _] -> x
-    | _ :: tail -> secondLastElement tail
+  | [] | [_] -> failwith "fewer than two elements"
+  | [x; _] -> x
+  | _ :: tail -> lastButOne tail
 
 printfn "last element: %d" <| last [ 1; 2; 3; 4; 5 ]
 printfn "last element: %d" <| last [ "a"; "bcd"; "efg" ]
